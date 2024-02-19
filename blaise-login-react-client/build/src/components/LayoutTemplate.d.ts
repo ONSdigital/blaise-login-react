09b67ae467e9ea777fdf0cd74c5d2065b475f167
jamie kerr <jamiestephenkerr@gmail.com>
2024-02-19 15:58:34 +0000

    ho hum

--- blaise-login-react-client/build/src/components/LayoutTemplate.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-interface LayoutTemplateProps {
-    title: string;
-    children: React.ReactNode;
-}
-export default function LayoutTemplate({ title, children }: LayoutTemplateProps): React.JSX.Element;
-export {};
