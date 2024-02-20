5c9d3c036591d688e17061660736844d5d0b0c35
jamie kerr <jamiestephenkerr@gmail.com>
2024-02-20 11:33:45 +0000

    Update header template for login

--- blaise-login-react-client/src/components/LayoutTemplate.tsx
+++ blaise-login-react-client/src/components/LayoutTemplate.tsx
@@ -1,4 +1,4 @@
-import { Footer, Header } from "blaise-design-system-react-components";
+import { Footer, Header, NotProductionWarning } from "blaise-design-system-react-components";
 import React from "react";
 
 const divStyle = {
@@ -10,17 +10,24 @@ interface LayoutTemplateProps {
   children: React.ReactNode;
 }
 
+function isProduction(hostname: string): boolean {
+  return hostname.endsWith(".blaise.gcp.onsdigital.uk");
+}
+
 export default function LayoutTemplate({ title, children}: LayoutTemplateProps) {
   return (
 
     <>
       <div data-testid="login-page">
-        <Header title={title} noSave/>
-  
-      <div style={divStyle} className="ons-page__container ons-container" data-testid="login-page-content">
-        {children}
-      </div>
-      <Footer />
+        <a className="ons-skip-link" href="#main-content">Skip to content</a>
+            {
+                isProduction(window.location.hostname) ? <></> : <NotProductionWarning />
+            }        
+        <Header title={title} noSave/>  
+        <div style={divStyle} className="ons-page__container ons-container" data-testid="login-page-content">
+          {children}
+        </div>
+        <Footer />
       </div>
     </>
   );
