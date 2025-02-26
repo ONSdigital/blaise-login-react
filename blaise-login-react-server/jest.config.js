716d71e8b501bf200e8715f1a4d959d0b7e0b992
kristian4res <57638182+kristian4res@users.noreply.github.com>
2025-02-26 13:37:17 +0000

    fix: resolve jest axios error

---
+++ blaise-login-react-server/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  preset: "ts-jest",
+  transform: {
+    "^.+\\.(ts|tsx)?$": "ts-jest",
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!(axios|blaise-api-node-client)/)"
+  ],
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+  ],
+  testMatch: [
+    "**/src/**/*.test.(ts|tsx|js|jsx)",
+  ],
+  modulePathIgnorePatterns: [
+    "<rootDir>/build/",
+    "<rootDir>/dist/",
+    "<rootDir>/lib/",
+  ],
+};
